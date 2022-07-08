5be9c90167150dcbffc7b917457312cfe6a80814
jorgecc <jcastro@eftec.cl>
2022-07-08 11:27:26 -0400

    julio 8

---
+++ src/test/java/com/example/julio8/Julio8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.julio8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Julio8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
